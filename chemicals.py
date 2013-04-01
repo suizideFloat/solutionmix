b709ac2820c3b21e44b6cf88afe9c95ae5f699ca
Conan the Vegetarian <git@etnch.org>
2013-04-01 18:58:01 +0200

    modified:   chemicals.py

--- chemicals.py
+++ chemicals.py
@@ -2,30 +2,33 @@
 # coding: utf8
 
 class GenericChemical(object):
+
     def __init__(self, mass, structur, dense, physcon):
-        self.name = __init__.name
-        self.mass = None
-        self.structur = None
-        self.dense = None
-        self.physcon = None
+        #self.name = __init__.name
+        self.mass = mass
+        self.structur = structur
+        self.dense = dense
+        self.physcon = physcon
+
+    def temp_to_loesen(self):
+        raise NotImplementedError(
+            'Methode temp_to_loesen nicht implementiert.'
+            )
+
 
 class NatriumChlorid(GenericChemical):
-    def __init__(self, mass, structur, dense, physcon):
-        GenericChemical.__init__(self, 58.44, 'NaCl', 2.17, 'solid') # Wichtige Zeile - siehe unten
+    def __init__(self):
+        GenericChemical.__init__(self, 58.44, 'NaCl', 2.17, 'solid')
     # def GenericChemical.__init__(self, 58.44, 'NaCl', 2.17, 'solid'):
-        self.mass = mass
-        self.structur = structur
-        self.dense = dense
-        self.physcon = physcon
+
+    def dummy_func(self):
+        return None
 
 
-class Salzsure(GenericChemical):
+class Salzsaeure(GenericChemical):
 
-    def __init__(self, mass, structur, dense, physcon):
-        GenericChemical.__init__(self)
-    # 36.46, 'HCl', 1.64, 'gaseous'
+    def __init__(self):
+        GenericChemical.__init__(self,36.46, 'HCl', 1.64, 'gaseous')
 
-        self.mass = mass
-        self.structur = structur
-        self.dense = dense
-        self.physcon = physcon
+    def dummy_func(self):
+        return None
